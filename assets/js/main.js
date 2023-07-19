69517912332ca527f0cb925c44af5f8638e43e56
sunghee <tjdgml211018@naver.com>
2023-07-19 09:50:13 +0900

    공통 모듈 분리

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-/* 검색창 제어 */
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', ' ');
-});
-
-
 /* 페이지 스크롤에 따른 요소 제어 */
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -157,8 +138,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-/* 올해 연도 계산 */
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
